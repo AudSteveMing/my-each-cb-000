5125b81e2166da85e1dc1ae310970e8946cf29a2
Steve Cordova <cordovas@bc.edu>
2020-07-15 01:07:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,13 @@
-def my_each(collection) # put argument(s) here
-  i = 0
-  while i < collection.length
-    yield(collection[i])
-    i+=1
-   end
-  collection
+# def my_each(collection) # put argument(s) here
+#   i = 0
+#   while i < collection.length
+#     yield(collection[i])
+#     i+=1
+#    end
+#   collection
+# end
+
+
+def my_each(collection) do |i|
+  puts i
 end
